ab68868d05b8f57e1ba35e819665b586b2e017e3
Techno Sandy <technosandyindia007@gmail.com>
2016-12-28 23:32:47 +0530

    Initial Commit for the mobile version of the TextTranslator repository - Hybrid application using Phonegap

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # TextTranslator-AndroidApp
+This is the mobile version of the TextTranslator Repository . 
+This is a Hybrid mobile application prototype made using Phonegap API .
---
+++ www/index.js
@@ -0,0 +1,17 @@
+    function translator(str) {
+        if (str.length == 0) {
+            document.getElementById("text").innerHTML = "";
+            return;
+        }
+        else {
+            var xmlhttp = new XMLHttpRequest();
+            xmlhttp.onreadystatechange = function () {
+                if (this.readyState == 4 && this.status == 200) {
+                    document.getElementById("translatedText").innerHTML = this.responseText.replace(/<(?:.|\n)*?>/gm, '').trim();
+                }
+            };
+            //xmlhttp.open("GET", "translate.php?q=" + str, true);
+            xmlhttp.open("GET", "http://sandeepbalramsatone.com/texttranslator/translate.php?q=" + str, true);
+            xmlhttp.send();
+        }
+    }
